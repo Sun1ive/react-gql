f13fbd8f476018e39748a1719c26912a45566304
Sun1ive <sunliveua@gmail.com>
2018-04-04 17:40:36 +0300

    upd

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
 const PORT = 4000;
 
+app.use('/graphql', graphqlHTTP({}), async (req, res) => {});
+
 /* eslint-disable no-console */
 app.listen(PORT, () => console.log('Server is running at', PORT));
-
