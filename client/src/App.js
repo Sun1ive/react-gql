c3cd8ce9b88144bf8ef2bf9afb9720877783a1ac
Sun1ive <sunliveua@gmail.com>
2018-04-18 16:14:58 +0300

    upd

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import BookList from "./components/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:8000"
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Ninja's reading list</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's reading list</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
