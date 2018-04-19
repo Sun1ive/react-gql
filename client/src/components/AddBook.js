acaae36198697a448608e9d93df628654857b79a
Sun1ive <sunliveua@gmail.com>
2018-04-19 13:05:44 +0300

    upd

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries';
 
-const AddBook = props => {
-  let authors = <option disabled>Authors loading...</option>;
-  if (props.data.authors) {
-    authors = props.data.authors.map(author => (
-      <option key={author.id} value={author.id}>
-        {author.name}
-      </option>
-    ));
-  }
-  return (
-    <form>
-      <div className="field">
-        <label>Book name:</label>
-        <input type="text" />
-      </div>
-      <div className="field">
-        <label>Genre:</label>
-        <input type="text" />
-      </div>
-      <div className="field">
-        <label>Author:</label>
-        <select>{authors}</select>
-      </div>
-      <button>Add Book</button>
-    </form>
-  );
-};
+class AddBook extends Component {
+	state = {
+		name: '',
+		genre: '',
+		authorId: ''
+	};
+
+	changeNameHandler = val => {
+		const oldState = { ...this.state };
+		oldState.name = val;
+		this.setState(oldState);
+	};
+
+	changeGenreHandler = val => {
+		const oldState = { ...this.state };
+		oldState.genre = val;
+		this.setState(oldState);
+	};
+
+	changeAuthorHandler = val => {
+		const oldState = { ...this.state };
+		oldState.authorId = val;
+		this.setState(oldState);
+	};
+
+	submitFormHandler = e => {
+		e.preventDefault();
+		console.log(e);
+	};
+
+	render() {
+		let authors = <option disabled>Authors loading...</option>;
+		if (this.props.data.authors) {
+			authors = this.props.data.authors.map(author => (
+				<option key={author.id} value={author.id}>
+					{author.name}
+				</option>
+			));
+		}
+		return (
+			<form onSubmit={e => this.submitFormHandler(e)}>
+				<div className="field">
+					<label>Book name:</label>
+					<input
+						type="text"
+						onChange={e => this.changeNameHandler(e.target.value)}
+					/>
+				</div>
+				<div className="field">
+					<label>Genre:</label>
+					<input
+						type="text"
+						onChange={e => this.changeGenreHandler(e.target.value)}
+					/>
+				</div>
+				<div className="field">
+					<label>Author:</label>
+					<select onChange={e => this.changeAuthorHandler(e.target.value)}>
+						<option>Select Author</option>
+						{authors}
+					</select>
+				</div>
+				<button>Add Book</button>
+			</form>
+		);
+	}
+}
 
 export default graphql(getAuthorsQuery)(AddBook);
